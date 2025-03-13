dd56ab3bdb637ca7915399743de6819b756a001b
GVISWESWAR <visweswarg13@gmail.com>
2025-03-13 18:05:44 +0530

    hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,11 +1,7 @@
-//File: hello.cpp
 #include <iostream>
 using namespace std;
 
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this  \n";
-  cout << "PES2UG22CS205\n";
-  return 0;
+int main() {
+    cout << "Hello, Jenkins! Webhook triggered successfully! S.D PES2UG22CS468" << endl;
+    return 0;
 }
